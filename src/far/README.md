7212efd51c29b14c66bf2f513927034baadf80f4
Nic30 <nic30@seznam.cz>
2019-01-02 19:07:30 +0100

    add FaR placeholder

---
+++ src/far/README.md
@@ -0,0 +1,29 @@
+
+List of red-black trees with with path compression
+
+# Red-black tree
+* approximately balanced binary tree
+* https://www.cs.usfca.edu/~galles/visualization/RedBlack.html
+
+* The root is always black.
+* Leaf nodes are black.
+* If a node is red, then both its children are black.
+* The number of black nodes on every path are same.
+
+## properties
+* O(log n) max path len, insert/delete/lookup T.
+
+## special operations
+* left/right rotation - restores properties of rb tree.
+```
+   B                                C
+ |   |       ---left rot. -->     |   |
+A     C                           B   E
+    |   |    <--right rot. --   |   |
+    D   E                       A   D
+```
+A < B < D < C < E
+
+
+# Path compression
+* http://theoryofprogramming.com/2016/11/15/compressed-trie-tree/
---
+++ src/far/far.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "../Simulation.h"
+#include <functional>
+
+class FaRFilter {
+	std::vector<Rule> rules;
+};
+
+
+class FaRClassifier: public PacketClassifier {
+public:
+	virtual void ConstructClassifier(const std::vector<Rule>& rules);
+	virtual int ClassifyAPacket(const Packet& packet);
+
+	virtual void DeleteRule(size_t index) {
+		throw std::runtime_error("Can't delete rules (not implemented).");
+	}
+	virtual void InsertRule(const Rule& rule) {
+		throw std::runtime_error("Can't insert rules (not implemented).");
+	}
+	virtual Memory MemSizeBytes() const {
+		throw std::runtime_error("not implemented.");
+	}
+	virtual int MemoryAccess() const {
+		throw std::runtime_error("not implemented.");
+	}
+	virtual size_t NumTables() const {
+		throw std::runtime_error("not implemented.");
+	}
+	virtual size_t RulesInTable(size_t tableIndex) const {
+		throw std::runtime_error("not implemented.");
+	}
+
+	virtual ~FaRClassifier() {
+	}
+private:
+	FaRFilter filter;
+};
