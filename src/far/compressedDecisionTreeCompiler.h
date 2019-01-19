2e783b655917fa0263050a502552dc9b1d548f86
Nic30 <nic30@seznam.cz>
2019-01-19 16:31:07 +0100

    CompressedDecisionTree::classify

--- src/far/compressedDecisionTreeCompiler.h
+++ src/far/compressedDecisionTreeCompiler.h
@@ -5,11 +5,16 @@
 
 template<typename rule_t>
 class CompressedDecisionTreeCompiler {
-	using ComprTree = CompressedDecisionTree<rule_t::size()>;
+	using ComprTree = CompressedDecisionTree<uint8_t, rule_t::size()>;
 
 	ComprTree compile(const LLRBTree<rule_t> & llrb_tree) {
 		// [TODO] check size expression
 		ComprTree c_tree(llrb_tree.size() * rule_t::size());
+		// How to check ranges?
+		// problem is that in the tree the end is not the next or parent node the end is
+		// in the node itself and it can contain the long value over multiple dimmensions
+        //
+
 
 	}
 
