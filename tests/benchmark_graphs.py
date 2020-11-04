b419280f79940dea20ec60dddba9c406dcbedd75
Nic30 <nic30@seznam.cz>
2020-11-04 16:41:53 +0100

    3d histogram overview graphs

--- tests/benchmark_graphs.py
+++ tests/benchmark_graphs.py
@@ -1,53 +1,77 @@
 # This import registers the 3D projection, but is otherwise unused.
+import json
 from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
+import os
 
+from benchmark import benchmarks
 import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.cm as cm
+import re
 
 if __name__ == "__main__":
-#    fig = plt.figure()
-#    ax = fig.add_subplot(111, projection='3d')
-#    
-#    _x = [1, 2, 3, 2, 5, 2, 6, 8, 6, 7]
-#    _y = [10, 10, 20, 50, 20, 20, 30, 10, 40, 50]
-#    _xx, _yy = np.meshgrid(_x, _y)
-#    x, y = _xx.ravel(), _yy.ravel()
-#    _z = np.array([105, 25, 26, 74, 39, 85, 74, 153, 52, 98] + 
-#                  list(range(10)))
-#    
-#    # There may be an easier way to do this, but I am not aware of it
-#    z = np.zeros(len(x))
-#    for i in range(1, len(x)):
-#        z[i] = _z[(i * len(_z)) // len(x)]
-#    
-#    bottom = np.zeros_like(z)
-#    width = depth = 1
-#    
-#    ax.bar3d(x, y, bottom, width, depth, z, shade=True)
-#    plt.show()
-
-
     # setup the figure and axes
-    fig = plt.figure(figsize=(8, 3))
-    ax1 = fig.add_subplot(121, projection='3d')
+    fig = plt.figure(figsize=(16, 16),  dpi=100)
+    ax1 = fig.add_subplot(1,1,1, projection='3d')
     #ax2 = fig.add_subplot(122, projection='3d')
     
-    # fake data
-    _x = np.arange(4)
-    _y = np.arange(5)
+    # (x, y): float
+    z_throughput = {}
+    # key = "ClassificationTime(s)"
+    key = "ConstructionTime(ms)" # classification per second
+    # cache misses
+    
+    
+    
+    for (alg, ruleset, result_dir) in benchmarks:
+        ruleset_name = os.path.basename(ruleset)
+        f_path = os.path.join(result_dir, f"{alg}_{ruleset_name}")
+        # if not isfile(f_path) or f_path.endswith(".png"):
+        #    continue
+        # alg, ruleset, nominal_rule_cnt = ruleset.split("_")
+        
+        try:
+            with open(f_path) as fp:
+                results = json.load(fp)
+                val = results[key]
+        except FileNotFoundError:
+            raise
+            #val = None
+        z_throughput[(alg, ruleset)] = val
+
+    z_throughput = {k: 0 if v is None else float(v) for k, v in z_throughput.items()}
+    x_algs = sorted(set(k[0] for k in z_throughput.keys()))
+    y_rulesets = sorted(set(k[1] for k in z_throughput.keys()))
+    # sort algs, slowest to highest x (background)
+    x_algs = sorted(x_algs, key=lambda a: -sum(x[1] for x in z_throughput.items() if x[0][0] == a ))
+    
+    _x = np.arange(len(x_algs))
+    _y = np.arange(len(y_rulesets))
     _xx, _yy = np.meshgrid(_x, _y)
     x, y = _xx.ravel(), _yy.ravel()
     
-    top = x + y
+    top = np.array([
+        [z_throughput[(alg, ruleset)] for alg in x_algs]
+        for ruleset in y_rulesets
+    ])
+    #print(top)
+    top = top.astype(np.float)
+    top = top.ravel()
     bottom = np.zeros_like(top)
     width = depth = 1
-    x_labels = ["a", "b", "c", "d"]
+    # x, y, z : array-like
+    #     The coordinates of the anchor point of the bars.
+    # dx, dy, dz : float or array-like
+    #     The width, depth, and height of the bars, respectively.
     ax1.bar3d(x, y, bottom, width, depth, top, shade=True)
-    ax1.set_title('Shaded')
-    ax1.set_xticks(range(len(x_labels)))
-    ax1.set_xticklabels(x_labels)
+    ax1.set_title(key)
+    ax1.set_zlabel(re.match(".*(\(.*\))", key).group(1).replace("[", "").replace(")", "]"))
+    #ax1.set_zscale("log")
+
+    ax1.set_xticks(range(len(x_algs)))
+    ax1.set_xticklabels(x_algs)
     
-    #ax2.bar3d(x, y, bottom, width, depth, top, shade=False)
-    #ax2.set_title('Not Shaded')
+    ax1.set_yticks(range(len(y_rulesets)))
+    ax1.set_yticklabels([os.path.basename(r) for r in y_rulesets])
     
     plt.show()
