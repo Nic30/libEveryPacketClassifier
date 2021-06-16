8f206d0912fd3a6c20d76e47a0a47b943f844af7
Nic30 <nic30@seznam.cz>
2021-06-16 23:37:23 +0200

    generate_rulesets: add time loging file

--- tests/generate_rulesets.py
+++ tests/generate_rulesets.py
@@ -1,7 +1,12 @@
-import os
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from datetime import datetime
 from multiprocessing import Pool, Value
-from subprocess import check_call
+import os
 from pathlib import Path
+from subprocess import check_call
+
 
 CLASSBENCH_ROOT = os.path.join(os.path.dirname(__file__), "..", "..", "classbench-ng")
 CLASSBENCH = os.path.join(os.path.dirname(__file__), "..", "..", "classbench-ng", "classbench")
@@ -9,6 +14,7 @@ EXACT_MATCH_RULE_GEN = os.path.join(os.path.dirname(__file__), "..", "..", "pcla
 RULESET_ROOT = os.path.join(CLASSBENCH_ROOT, "generated/")
 SEED_ROOT = os.path.join(CLASSBENCH_ROOT, "vendor", "parameter_files")
 OUT = os.path.join(CLASSBENCH_ROOT, "generated")
+OUT_TIME_LOG = os.path.join(CLASSBENCH_ROOT, "generated", "time.log")
 
 
 def format_num(n):
@@ -20,29 +26,29 @@ def format_num(n):
 
 SIZES = [
     # 100,
-    #500,
-    #1e3,
+    # 500,
+    # 1e3,
     2e3,
     5e3,
     10e3,
-    #65e3,
-    #100e3,
-    #1e3, 10e3, 100e3, 1e6, 10e6
+    65e3,
+    100e3,
+    # 1e3, 10e3, 100e3, 1e6, 10e6
 ]
 SEEDS = [os.path.join(SEED_ROOT, s) for s in [
-    #"exact",
+    # "exact",
     "acl1_seed",
     "acl2_seed",
-    #"acl3_seed",
-    #"acl4_seed",
-    #"acl5_seed",
-    #"fw1_seed",
-    #"fw2_seed",
-    #"fw3_seed",
-    #"fw4_seed",
-    #"fw5_seed",
-    #"ipc1_seed",
-    #"ipc2_seed",
+    # "acl3_seed",
+    # "acl4_seed",
+    # "acl5_seed",
+    # "fw1_seed",
+    # "fw2_seed",
+    # "fw3_seed",
+    # "fw4_seed",
+    # "fw5_seed",
+    # "ipc1_seed",
+    # "ipc2_seed",
 ]]
 
 counter = None
@@ -64,13 +70,21 @@ def run_classbench(args):
     res_f_path = Path(res_f)
     if not res_f_path.is_file() or res_f_path.stat().st_size == 0:
         with open(res_f, "w") as f:
+            print(f"Calling classbench generator for {seed} {size}")
+            t0 = datetime.now()
             check_call([CLASSBENCH, "generate", "v4", "--count=%d" % (size), seed], stdout=f, cwd=CLASSBENCH_ROOT)
-            with counter.get_lock():
-                counter.value += 1
-            print("%.2f%% %r" % (counter.value / len(tasks) * 100, args))
-    else:
+            t1 = datetime.now()
         with counter.get_lock():
             counter.value += 1
+        print("%.2f%% %r" % (counter.value / len(tasks) * 100, args))
+        with open(OUT_TIME_LOG, 'a+') as f:
+            print(f"classbench {seed} {size} {str(t1-t0):s}")
+
+    else:
+        print(f"classbench {seed} {size} already exits")
+        with counter.get_lock():
+            counter.value += 1
+
 
 def run_exact_gen(args):
     global counter
@@ -81,11 +95,20 @@ def run_exact_gen(args):
     res_f_path = Path(res_f)
     if not res_f_path.is_file() or res_f_path.stat().st_size == 0:
         with open(res_f, "w") as f:
+            print(f"Calling exactmatch generator for {seed} {size}")
+            t0 = datetime.now()
             check_call([EXACT_MATCH_RULE_GEN, str(seed), str(size)], stdout=f, cwd=CLASSBENCH_ROOT)
-            with counter.get_lock():
-                counter.value += 1
-            print("%.2f%% %r" % (counter.value / len(SIZES) * 100, args))
+            t1 = datetime.now()
+
+        with counter.get_lock():
+            counter.value += 1
+
+        print("%.2f%% %r" % (counter.value / len(SIZES) * 100, args))
+        with open(OUT_TIME_LOG, 'a+') as f:
+            print(f"classbench {seed} {size} {str(t1-t0):s}")
+
     else:
+        print(f"exactmatch {seed} {size} already exits")
         with counter.get_lock():
             counter.value += 1
 
@@ -93,12 +116,12 @@ def run_exact_gen(args):
 if __name__ == "__main__":
     counter = Value('i', 0)
     with Pool(14, initializer=init, initargs=(counter,)) as pool:
-        i = pool.map_async(run_classbench, tasks, chunksize=1)
+        i = pool.map_async(run_classbench, tasks)
         i.wait()
         print(i.get())
 
-    #_tasks = [(0, size) for size in SIZES]
-    #with Pool( initializer=init, initargs=(counter,)) as pool:
+    # _tasks = [(0, size) for size in SIZES]
+    # with Pool( initializer=init, initargs=(counter,)) as pool:
     #    i = pool.map_async(run_exact_gen, _tasks, chunksize=1)
     #    i.wait()
     #    print(i.get())
